6e482f62da14c910a0ee165136b78e157ec51328
Scott Miller <wsmiller324@gmail.com>
2015-06-27 20:00:41 -0700

    getting that tableviewcontroller figured out

--- UITableViewController Challenge Solution/TableViewController.h
+++ UITableViewController Challenge Solution/TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Scotty on 6/27/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
